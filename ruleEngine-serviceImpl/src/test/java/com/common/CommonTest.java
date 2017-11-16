16a4b5dcf20ea57e77579569c259110f7ac5d6e7
yuan.yuan1 <yuan.yuan1@baozun.com>
2017-11-16 15:16:23 +0800

    rm duplicate dependency

--- ruleEngine-serviceImpl/src/test/java/com/common/CommonTest.java
+++ ruleEngine-serviceImpl/src/test/java/com/common/CommonTest.java
@@ -1,5 +1,7 @@
 package com.common;
 
+import com.alibaba.fastjson.JSON;
+import com.engine.entity.task.Incident;
 import org.junit.Test;
 
 public class CommonTest {
@@ -8,6 +10,8 @@ public class CommonTest {
     public void test(){
         int i=1, j=0;
         switch (i){
+            case 1:
+                j+=100;
             case 2:
                 j+=6;
             case 0:
@@ -20,5 +24,13 @@ public class CommonTest {
                 j+=1;
         }
         System.out.println(j);
+    }
+
+    @Test
+    public void testFast(){
+        Incident incident = new Incident();
+        incident.setDocNum("hh");
+        String s = JSON.toJSONString(incident);
+        System.out.println(s);
     }
 }
