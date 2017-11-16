8e1e945b4abe77deb41bb4c924fb7ec708616bed
yuan.yuan1 <yuan.yuan1@baozun.com>
2017-11-16 10:22:58 +0800

    mybatis 批量插入

--- ruleEngine-serviceImpl/src/main/java/com/engine/dao/HumanTaskDao.java
+++ ruleEngine-serviceImpl/src/main/java/com/engine/dao/HumanTaskDao.java
@@ -1,8 +1,15 @@
 package com.engine.dao;
 
 import com.engine.entity.task.HumanTask;
+import com.engine.entity.task.Incident;
+
+import java.util.List;
 
 public interface HumanTaskDao {
 
     void insertHumanTask(HumanTask humanTask);
+
+    void insertIncident(Incident incident);
+
+    void batchInsertIncident(List<Incident> list);
 }
--- ruleEngine-serviceImpl/src/test/java/com/humanTask/TestHumanTaskTest.java
+++ ruleEngine-serviceImpl/src/test/java/com/humanTask/TestHumanTaskTest.java
@@ -3,11 +3,16 @@ package com.humanTask;
 
 import com.engine.dao.HumanTaskDao;
 import com.engine.entity.task.HumanTask;
+import com.engine.entity.task.Incident;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.TestPropertySource;
 import org.springframework.test.context.junit4.SpringRunner;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @RunWith(SpringRunner.class)
 @ContextConfiguration(locations={"classpath*:spring/spring.xml"})
@@ -18,11 +23,29 @@ public class TestHumanTaskTest {
 
     @Test
     public void testInsert(){
-
         HumanTask humanTask = new HumanTask();
         humanTask.setDocNum("test");
         humanTaskDao.insertHumanTask(humanTask);
-
-
     }
+
+
+    @Test
+    public void testInsertIncident(){
+        Incident incident = new Incident();
+        incident.setDocNum("incident");
+        humanTaskDao.insertIncident(incident);
+    }
+
+    @Test
+    public void testBatchInsert(){
+        List<Incident> list = new ArrayList<>();
+        for(int i =0; i < 10; i++){
+            Incident incident = new Incident();
+            incident.setDocNum("incident");
+            list.add(incident);
+        }
+        humanTaskDao.batchInsertIncident(list);
+    }
+
+
 }
