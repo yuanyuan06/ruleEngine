663338eb82a2cb5b7038e2b6903a6198049e244a
yuan.yuan1 <yuan.yuan1@baozun.com>
2017-11-23 11:13:31 +0800

    菜单按钮后台维护

--- web/src/main/java/com/engine/controller/EngineController.java
+++ web/src/main/java/com/engine/controller/EngineController.java
@@ -7,7 +7,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.servlet.ModelAndView;
 
-import javax.jws.WebParam;
+import java.io.Serializable;
 import java.util.*;
 
 @Controller
@@ -19,8 +19,65 @@ public class EngineController {
     @ResponseBody
     @RequestMapping("index")
     public ModelAndView index(){
-        return new ModelAndView("index");
+
+        Menu menu = new Menu();
+        menu.setUrl("/engine/readme.htm");
+        menu.setValue("hello world");
+
+        Menu menu1 = new Menu();
+        menu1.setUrl("/engine/snippet.htm");
+        menu1.setValue("编写规则片段");
+
+        Menu menu2 = new Menu();
+        menu2.setUrl("/engine/stage.htm");
+        menu2.setValue("设置stage");
+
+        Menu menu3 = new Menu();
+        menu3.setUrl("/engine/action.htm");
+        menu3.setValue("配置action");
+
+        Menu menu4 = new Menu();
+        menu4.setUrl("/engine/apply.htm");
+        menu4.setValue("应用规则");
+
+        List<Menu> list = new ArrayList<>();
+            list.add(menu);
+            list.add(menu1);
+            list.add(menu2);
+            list.add(menu3);
+            list.add(menu4);
+            Map<String, Object> map = new HashMap<>();
+        map.put("menus", list);
+
+        return new ModelAndView("index", map);
     }
+
+    class Menu implements Serializable{
+        private static final long serialVersionUID = 2208112916395973913L;
+
+        private String value;
+        private String url;
+
+        public String getValue() {
+            return value;
+        }
+
+        public void setValue(String value) {
+            this.value = value;
+        }
+
+        public String getUrl() {
+            return url;
+        }
+
+        public void setUrl(String url) {
+            this.url = url;
+        }
+    }
+
+
+
+
 
     @ResponseBody
     @RequestMapping("apply")
